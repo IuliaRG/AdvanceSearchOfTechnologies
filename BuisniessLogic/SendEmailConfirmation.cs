62ea555e74f7ef7910e6b7a7bf677979dfcca381
Iulia <iulia_rad19@yahoo.com>
2018-01-03 21:35:45 +0200

    email confirmation

--- BuisniessLogic/SendEmailConfirmation.cs
+++ BuisniessLogic/SendEmailConfirmation.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace BuisniessLogic
 {
-   public  class EmailConfirmation : EmailService
+   public  class SendEmailConfirmation : EmailService
     {
         private string email ;
         private string linkForConfirmation;
+        private string cxc;
         public override string TextSubject { get { return "Confirm email";  } }
         public override string TextBody { get { return "Press link to confirm your email "; } }
         public override string EmailTo { get { return email; } }
         public override string Link { get { return linkForConfirmation; } }
-        public EmailConfirmation() : base()
-        { }
-        public override void SendEmailConfirmation(string link, string emailAddress)
+        public SendEmailConfirmation(string link, string emailAddress) : base()
         {
             linkForConfirmation = link;
-             email = emailAddress;
+            email = emailAddress;
         }
+       
 
     }
 }

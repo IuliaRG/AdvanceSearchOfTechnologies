55ff96716b0e03e989a7a1d32288d5fc7d3a9f71
Iulia <iulia_rad19@yahoo.com>
2018-03-10 17:44:11 +0200

    user review

---
+++ Abstracts/ITextAnalyticsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts
+{
+    public interface ITextAnalyticsService
+    {
+        IEnumerable<string> GetKeyPhrases(string text);
+
+      //  ResponseLanguage GetLanguage(string languageText);
+
+      //  ResponseSentiment GetSentiment(string sentimentText);
+
+      string SerializeText(string text);
+        
+
+    }
+}
